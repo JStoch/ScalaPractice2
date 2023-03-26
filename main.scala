0db09c53b6c9853a6e061108a5674ca68afb170c
JStoch <95025604+JStoch@users.noreply.github.com>
2023-03-26 17:43:50 +0200

    Added comments

--- main.scala
+++ main.scala
@@ -4,27 +4,13 @@ import java.awt.Color
   
 }
 
-//zad.0
-def log(prefix: String)(datetime: String)(text: String) = {
-  println("[" + prefix + "] " + datetime + " \t " + text)
-}
-
-def color (s: String): String = {
-  s match
-    case "WARN" => Console.YELLOW + "WARN" + Console.RESET
-    case "INFO" => Console.BLUE + "INFO" + Console.RESET
-    case "DEBUG" => Console.GREEN + "DEBUG" + Console.RESET
-    case "CRITICAL" => Console.RED + "CRITICAL" + Console.RESET
-}
-
-//zad.1
+//my implementation of functions for operating on lists
 def map[A](list: List[A], fun: A => A): List[A] = {
   list match
     case Nil => list
     case _ => fun(list.head)::map(list.tail, fun)
 }
 
-//zad.2
 def filter[A](list: List[A], pred: A => Boolean): List[A] = {
     list match
       case Nil => list
@@ -34,33 +20,33 @@ def filter[A](list: List[A], pred: A => Boolean): List[A] = {
 
 }
 
-//zad. 3
 def reduce[A](list: List[A], op: (Int, A) => Int, acc: Int): Int = {
   list match
     case Nil => acc
     case _ => reduce(list.tail, op, op(acc, list.head))
 }
 
-//zad. 4
+//calculate the average value
 def average(list: List[Int]): Float = {
   val op = (a: Int, b: Int) => a+b
   reduce(list, op, 0).toFloat / list.length
 }
 
-//zad. 5
+//make an acronym from capital letters in the string
 def acronymWithCapital(s: String): String = {
   val sList = s.toList
   val pred = (c: Char) => c >= 65 && c <= 90
   filter(sList, pred).mkString("")
 }
 
+//make an acronym from first letters of words
 def acronymWithSpaces(s: String): String = {
   val sList = s.split(' ').toList
   val firstLetter = (s: String) => s.charAt(0).toString
   map(sList, firstLetter).mkString("")
 }
 
-//zad. 6
+//return a list of squared numbers, which raised to the 3rd power are lower than the sum of all numbers in the list
 def filteredSquare(list: List[Int]): List[Int] = {
   val op = (a: Int, b: Int) => a + b
   val sum = reduce(list, op, 0)
